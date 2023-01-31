101589cbb462e84006e27a6050af4a5694fb98fa
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-01-31 15:04:13 +0300

    click-to-change

---
+++ app.js
@@ -0,0 +1,46 @@
+let cards = document.querySelectorAll('.cat_card');
+
+for (let card of cards) {
+    let cardId = card.id;
+    let possibleTextOptions = ['Печень утки разварная с артишоками.', 'Головы щучьи с чесноком да свежайшая сёмгушка.', 'Филе из цыплят с трюфелями в бульоне.']
+    let disabledText = ['Печалька, с фуа-гра закончился.', 'Печалька, с рыбой закончился.', 'Печалька, с курой закончился.']
+    let informationBlock = card.querySelector('.information_block');
+    let bottomCardText = card.querySelector('.bottom_card_text');
+    let topText = card.querySelector('.text_1');
+    let link = bottomCardText.querySelector('.card_link')
+
+    
+
+
+
+    isDisabled = function() {
+        if (informationBlock.classList.contains('disabled')) {
+            bottomCardText.textContent = disabledText[cardId]
+            bottomCardText.style.color = '#ffff66';
+        }        
+    }
+    
+    isDisabled()
+
+    
+    informationBlock.onclick = function() {
+        if (informationBlock.classList.contains('disabled')) {
+            return;
+        } else {
+            informationBlock.classList.toggle('active');
+            // bottomCardText.textContent = possibleTextOptions[cardId]            
+            bottomCardText.classList.toggle('selected_text')
+            
+                       
+        } 
+    }
+
+    // const toggleText = function () {
+    //     if (informationBlock.classList.contains('active')) {
+    //         bottomCardText.textContent = possibleTextOptions[cardId]
+    // }
+    
+    // }
+
+}
+
